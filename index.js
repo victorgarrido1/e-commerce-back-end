102623ba1e2550622ba4c39e1836ee7f042237dd
victorgarrido1 <vcgarr94@gmail.com>
2024-01-28 23:01:24 -0700

    add inquirer questions

--- index.js
+++ index.js
@@ -1,10 +1,55 @@
 const router = require('express').Router();
+const inquirer = require('inquirer'); // Changed from destructuring import
 const apiRoutes = require('./api');
 
 router.use('/api', apiRoutes);
 
 router.use((req, res) => {
-  res.send("<h1>Wrong Route!</h1>")
+  res.send("<h1>Wrong Route!</h1>");
 });
+
+// Function that uses Inquirer to prompt the user for the command line
+function promptUser() {
+  inquirer.prompt([
+    {
+      type: 'list',
+      name: 'choices',
+      message: 'What would you like to do?',
+      choices: [
+        'View all Employees',
+        'Add Employee',
+        'Update Employee',
+        'View All Roles',
+        'Add Role',
+        'View All Departments',
+        'Add Department'
+      ]
+    }
+  ]).then(answers => {
+    const { choices } = answers;
+
+    if (choices === 'View All Employees') {
+      showEmployees();
+    }
+    if (choices === 'Add Employee') {
+      addEmployee();
+    }
+    if (choices === 'View All Roles') {
+      showARoles();
+    }
+    if (choices === 'Add Role') {
+      addRole();
+    }
+    if (choices === 'View All Departments') {
+      viewADepartments();
+    }
+    if (choices === 'Add Department') {
+      addDepartment();
+    }
+
+    // Process user's answers here
+    console.log(answers);
+  });
+}
 
 module.exports = router;
