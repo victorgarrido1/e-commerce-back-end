bf250e7a27ace1a7f30a2385b56ff5901206ba88
victorgarrido1 <vcgarr94@gmail.com>
2024-02-04 15:22:23 -0700

    applying routes to product and tag

--- routes/api/tag-routes.js
+++ routes/api/tag-routes.js
@@ -21,7 +21,7 @@ router.get('/:id',async (req, res) => {
   try {
     const tagData = await Tag.findByPk(req.params.id, {
       include: [{ model: Product}],
-    }),
+    })
     if (!tagData) {
       res.status(404).json({ message: "Tag is not found with this id."});
       return;
@@ -34,7 +34,7 @@ router.get('/:id',async (req, res) => {
 });
 
 // create a new tag
-router.post('/',async (req, res) => {
+router.post('/', async (req, res) => {
   try {
    const tagData = await Tag.create(req.body);
    res.status(200).json(tagData);
@@ -44,14 +44,27 @@ router.post('/',async (req, res) => {
 });
 
 // update a tag's name by its `id` value
-router.put('/:id', (req, res) => {
+router.put('/:id',async (req, res) => {
   try{
-  
+    const tagData = await Tag.create(req.body);
+    res.status(200).json(tagData);
+  } catch (err) {
+    res.status(400).json({ message: "The tag creation was failed "});
   }
 });
 
-router.delete('/:id', (req, res) => {
-  // delete on tag by its `id` value
+router.delete('/:id', async (req, res) => {
+  // Delete a tag by its `id` value
+  try {
+    const deleted = await Tag.destroy({ where: { id: req.params.id } });
+
+    deleted
+      ? res.status(200).json({ message: "Tag deleted successfully" })
+      : res.status(404).json({ message: "No tag was found with this id" });
+  } catch (err) {
+    res.status(500).json({ message: "Tag deletion has failed" });
+  }
 });
+
 
 module.exports = router;
